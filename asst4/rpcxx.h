4b2abdb5ab563c0279f131ea8bfc94bde9b7d294
nadiakostiv <nadiakostiv98@gmail.com>
2020-12-17 12:45:14 -0500

    Fix compilation errors for test-complex

--- asst4/rpcxx.h
+++ asst4/rpcxx.h
@@ -2,6 +2,8 @@
 #ifndef RPCXX_H
 #define RPCXX_H
 
+#include <iostream>
+#include <typeinfo>
 #include <cstdlib>
 #include "rpc.h"
 
@@ -119,6 +121,17 @@ class IntResult : public BaseResult {
     int &data() { return r; }
 };
 
+template<typename T>
+class Result {
+  T r;
+public:
+  T &data() { return r; }
+};
+
+template<>
+class Result<void> {};
+
+
 // TASK2: Client-side
 class Client : public BaseClient {
  public:
@@ -141,6 +154,15 @@ class Client : public BaseClient {
 	}
 	return result;
     }
+    /* add this */
+    template<typename Svc, typename RT, typename ... FA> 
+    Result<RT> * Call(Svc *svc, RT (Svc::*f)(FA...), ...) {
+      std::cout << "WARNING: Calling " 
+            << typeid(decltype(f)).name()
+            << " is not supported\n";
+      return nullptr;
+    }
+    /* end here */
 };
 
 // TASK2: Server-side
@@ -150,6 +172,14 @@ class Service : public BaseService {
   	void Export(int (Svc::*func)(int)) {
 	ExportRaw(MemberFunctionPtr::From(func), new IntIntProcedure<Svc>());
     }
+    /* add this */
+    template<typename MemberFunction>
+    void Export(MemberFunction f) {
+      std::cout << "WARNING: Exporting " 
+                << typeid(MemberFunction).name()
+                << " is not supported\n";
+    }
+    /* end here */
 };
 
 }
