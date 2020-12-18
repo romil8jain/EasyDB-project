1fdd066f810b2f316cce065b3cf1199d1ff1fcb4
romil8jain <57597689+romil8jain@users.noreply.github.com>
2020-12-19 02:19:09 +0530

    Made major changes to finish server

--- asst4/rpcxx.h
+++ asst4/rpcxx.h
@@ -10,9 +10,6 @@
 namespace rpc {
   
 // Protocol is used for encode and decode a type to/from the network.
-//
-// You may use network byte order, but it's optional. We won't test your code
-// on two different architectures.
  
 // TASK1: add more specializations to Protocol template class to support more
 // types.
@@ -58,21 +55,22 @@ template <typename T> struct Protocol {
    }
 };
  
-template <typename T> struct ProtocolUniversal {
+template <typename T1, typename T2> struct ProtocolUniversal {
   
-   static constexpr size_t TYPE_SIZE = sizeof(T);
+   static constexpr size_t TYPE_SIZE1 = sizeof(T1);
+   static constexpr size_t TYPE_SIZE2 = sizeof(T2);
 
-   static bool Decode(uint8_t *in_bytes, uint32_t *in_len, bool *ok, T &x) {
+
+   static bool Decode(uint8_t *in_bytes, uint32_t *in_len, bool *ok, T1 &x, T2 &y) {
    // check if buffer is big enough to read in x, if not, return false
-       if (*in_len < TYPE_SIZE) return false;
+       if (*in_len < (TYPE_SIZE1 + TYPE_SIZE2)) return false;
  
        // do a memory copy from the buffer into the data, TYPE_SIZE is the size of the data
-       memcpy(&x, in_bytes, TYPE_SIZE);
-
-	   in_bytes+=TYPE_SIZE;
+       memcpy(&x, in_bytes, TYPE_SIZE1);
+	   memcpy(&y, in_bytes + TYPE_SIZE1, TYPE_SIZE2)
  
        // since we consumed TYPE_SIZE number of bytes from the buffer, we set *in_len to TYPE_SIZE
-       *in_len = *in_len - TYPE_SIZE;
+       *in_len = TYPE_SIZE1 + TYPE_SIZE2;
  
        return true;
    }
@@ -141,7 +139,7 @@ class IntIntProcedure : public BaseProcedure {
    bool DecodeAndExecute(uint8_t *in_bytes, uint32_t *in_len,
                        uint8_t *out_bytes, uint32_t *out_len,
                        bool *ok) override final { 
-                        //override means don’t allow derived class to override the base class’ virtual function
+
 		int x;
 		// This function is similar to Decode. We need to return false if buffer
 		// isn't large enough, or fatal error happens during parsing.
@@ -169,37 +167,112 @@ class IntIntProcedure : public BaseProcedure {
 };
 
 
-// TASK2: Server-side: Trying to make universal procedure (probably not for strings)
-template <typename Svc, typename RT, typename ... Args>
-class UniversalProcedure : public BaseProcedure {
+// TASK2: Server-side SPECIALIZATION
 
-	template<typename T, typename ... Vargs> 
-	bool DecodeRecursive(uint8_t *in_bytes, uint32_t *in_len,
+// void with return void implementation
+template <typename Svc>
+class UniversalProcedure : public BaseProcedure {
+   bool DecodeAndExecute(uint8_t *in_bytes, uint32_t *in_len,
                        uint8_t *out_bytes, uint32_t *out_len,
-                       bool *ok,  ... &finalargs)  {  // passing by reference here?
-                        //override means don’t allow derived class to override the base class’ virtual function
-		T x;
+                       bool *ok) override final { 
+
+
+		*in_len = 0;
+		// Now we cast the function pointer func_ptr to its original type.
+		//
+		// This incomplete solution only works for this type of member functions.
+		using FunctionPointerType = void (Svc::*)();
+		auto p = func_ptr.To<FunctionPointerType>();
+
+		// EXECUTION STEP: we call the function of the Svc class here and get the result and encode the result
+		(((Svc *) instance)->*p)(); // The svc class here inherits from the service class so typecasting is allowed
+
+		return true;
+   }
+};
+
+//void with 2 parameters
+template <typename Svc, void, typename T1, typename T2>
+class UniversalProcedure : public BaseProcedure {
+   bool DecodeAndExecute(uint8_t *in_bytes, uint32_t *in_len,
+                       uint8_t *out_bytes, uint32_t *out_len,
+                       bool *ok) override final { 
+
+
+		T1 x;
+		T2 y;
 		// This function is similar to Decode. We need to return false if buffer
 		// isn't large enough, or fatal error happens during parsing.
-		if (!ProtocolUniversal<T>::Decode(in_bytes, in_len, ok, x) || !*ok) {
+		if (!ProtocolUniversal<T1, T2>::Decode(in_bytes, in_len, ok, x, y) || !*ok) {
 			return false;
 		}
-		return DecodeRecursive<Vargs ...>(uint8_t *in_bytes, uint32_t *in_len,
-                       uint8_t *out_bytes, uint32_t *out_len,
-                       bool *ok, finalargs ..., x);
+
+		// Now we cast the function pointer func_ptr to its original type.
+		//
+		// This incomplete solution only works for this type of member functions.
+		using FunctionPointerType = void (Svc::*)(T1, T2);
+		auto p = func_ptr.To<FunctionPointerType>();
+
+		// EXECUTION STEP: we call the function of the Svc class here and get the result and encode the result
+		(((Svc *) instance)->*p)(x, y); // The svc class here inherits from the service class so typecasting is allowed
+
+		return true;
    }
+};
 
-	template<> 
-    bool DecodeRecursive(uint8_t *in_bytes, uint32_t *in_len,
+//void with 1 strings
+template <typename Svc, void, std::string, typename T2>
+class UniversalProcedure : public BaseProcedure {
+   bool DecodeAndExecute(uint8_t *in_bytes, uint32_t *in_len,
                        uint8_t *out_bytes, uint32_t *out_len,
-                       bool *ok, ... &finalargs)  { 
+                       bool *ok) override final { 
 
-		using FunctionPointerType = RT (Svc::*)(finalargs ...);
+
+		std::string x;
+		T2 y;
+		// This function is similar to Decode. We need to return false if buffer
+		// isn't large enough, or fatal error happens during parsing.
+		if (!ProtocolUniversal<std::string, T2>::Decode(in_bytes, in_len, ok, x, y) || !*ok) {
+			return false;
+		}
+
+		// Now we cast the function pointer func_ptr to its original type.
+		//
+		// This incomplete solution only works for this type of member functions.
+		using FunctionPointerType = void (Svc::*)(std::string, T2);
+		auto p = func_ptr.To<FunctionPointerType>();
+
+		// EXECUTION STEP: we call the function of the Svc class here and get the result and encode the result
+		(((Svc *) instance)->*p)(x, y); // The svc class here inherits from the service class so typecasting is allowed
+
+		return true;
+   }
+};
+
+// 1 params and a return type: non string
+template <typename Svc, typename RT, typename T1>
+class UniversalProcedure : public BaseProcedure {
+   bool DecodeAndExecute(uint8_t *in_bytes, uint32_t *in_len,
+                       uint8_t *out_bytes, uint32_t *out_len,
+                       bool *ok) override final { 
+
+
+		T1 x;
+		// This function is similar to Decode. We need to return false if buffer
+		// isn't large enough, or fatal error happens during parsing.
+		if (!ProtocolUniversal<T1>::Decode(in_bytes, in_len, ok, x) || !*ok) {
+			return false;
+		}
+
+		// Now we cast the function pointer func_ptr to its original type.
+		//
+		// This incomplete solution only works for this type of member functions.
+		using FunctionPointerType = RT (Svc::*)(T1);
 		auto p = func_ptr.To<FunctionPointerType>();
 
 		// EXECUTION STEP: we call the function of the Svc class here and get the result and encode the result
 		RT result = (((Svc *) instance)->*p)(x); // The svc class here inherits from the service class so typecasting is allowed
-		
+
 		if (!Protocol<RT>::Encode(out_bytes, out_len, result)) {
 			// out_len should always be large enough so this branch shouldn't be
 			// taken. However just in case, we return an fatal error by setting *ok
@@ -209,16 +282,147 @@ class UniversalProcedure : public BaseProcedure {
 		}
 		return true;
    }
-   		
+};
+
+
+// 2 params and a return type: non string
+template <typename Svc, typename RT, typename T1, typename T2>
+class UniversalProcedure : public BaseProcedure {
    bool DecodeAndExecute(uint8_t *in_bytes, uint32_t *in_len,
                        uint8_t *out_bytes, uint32_t *out_len,
                        bool *ok) override final { 
 
-		return DecodeRecursive<Args ...>(uint8_t *in_bytes, uint32_t *in_len,
-                       uint8_t *out_bytes, uint32_t *out_len,
-                       bool *ok);
 
-		
+		T1 x;
+		T2 y;
+		// This function is similar to Decode. We need to return false if buffer
+		// isn't large enough, or fatal error happens during parsing.
+		if (!ProtocolUniversal<T1, T2>::Decode(in_bytes, in_len, ok, x, y) || !*ok) {
+			return false;
+		}
+
+		// Now we cast the function pointer func_ptr to its original type.
+		//
+		// This incomplete solution only works for this type of member functions.
+		using FunctionPointerType = RT (Svc::*)(T1, T2);
+		auto p = func_ptr.To<FunctionPointerType>();
+
+		// EXECUTION STEP: we call the function of the Svc class here and get the result and encode the result
+		RT result = (((Svc *) instance)->*p)(x, y); // The svc class here inherits from the service class so typecasting is allowed
+
+		if (!Protocol<RT>::Encode(out_bytes, out_len, result)) {
+			// out_len should always be large enough so this branch shouldn't be
+			// taken. However just in case, we return an fatal error by setting *ok
+			// to false.
+			*ok = false;
+			return false;
+		}
+		return true;
+   }
+};
+
+// 2 params and a return type: first string
+template <typename Svc, typename RT, std::string, typename T2>
+class UniversalProcedure : public BaseProcedure {
+   bool DecodeAndExecute(uint8_t *in_bytes, uint32_t *in_len,
+                       uint8_t *out_bytes, uint32_t *out_len,
+                       bool *ok) override final { 
+
+		std::string x;				   
+		T2 y;
+		// This function is similar to Decode. We need to return false if buffer
+		// isn't large enough, or fatal error happens during parsing.
+		if (!ProtocolUniversal<std::string, T2>::Decode(in_bytes, in_len, ok, x, y) || !*ok) {
+			return false;
+		}
+
+		// Now we cast the function pointer func_ptr to its original type.
+		//
+		// This incomplete solution only works for this type of member functions.
+		using FunctionPointerType = RT (Svc::*)(std::string, T2);
+		auto p = func_ptr.To<FunctionPointerType>();
+
+		// EXECUTION STEP: we call the function of the Svc class here and get the result and encode the result
+		RT result = (((Svc *) instance)->*p)(x, y); // The svc class here inherits from the service class so typecasting is allowed
+
+		if (!Protocol<RT>::Encode(out_bytes, out_len, result)) {
+			// out_len should always be large enough so this branch shouldn't be
+			// taken. However just in case, we return an fatal error by setting *ok
+			// to false.
+			*ok = false;
+			return false;
+		}
+		return true;
+   }
+};
+
+// 2 params and a return type: second string
+template <typename Svc, typename RT, typename T2, std::string>
+class UniversalProcedure : public BaseProcedure {
+   bool DecodeAndExecute(uint8_t *in_bytes, uint32_t *in_len,
+                       uint8_t *out_bytes, uint32_t *out_len,
+                       bool *ok) override final { 
+			   
+		T1 x;
+		std::string y;
+		// This function is similar to Decode. We need to return false if buffer
+		// isn't large enough, or fatal error happens during parsing.
+		if (!ProtocolUniversal<T1, std::string>::Decode(in_bytes, in_len, ok, x, y) || !*ok) {
+			return false;
+		}
+
+		// Now we cast the function pointer func_ptr to its original type.
+		//
+		// This incomplete solution only works for this type of member functions.
+		using FunctionPointerType = RT (Svc::*)(T1, std::string);
+		auto p = func_ptr.To<FunctionPointerType>();
+
+		// EXECUTION STEP: we call the function of the Svc class here and get the result and encode the result
+		RT result = (((Svc *) instance)->*p)(x, y); // The svc class here inherits from the service class so typecasting is allowed
+
+		if (!Protocol<RT>::Encode(out_bytes, out_len, result)) {
+			// out_len should always be large enough so this branch shouldn't be
+			// taken. However just in case, we return an fatal error by setting *ok
+			// to false.
+			*ok = false;
+			return false;
+		}
+		return true;
+   }
+};
+
+// 2 params and a return type: second string
+template <typename Svc, typename RT, std::string, std::string>
+class UniversalProcedure : public BaseProcedure {
+   bool DecodeAndExecute(uint8_t *in_bytes, uint32_t *in_len,
+                       uint8_t *out_bytes, uint32_t *out_len,
+                       bool *ok) override final { 
+			   
+		std::string x;
+		std::string y;
+		// This function is similar to Decode. We need to return false if buffer
+		// isn't large enough, or fatal error happens during parsing.
+		if (!ProtocolUniversal<std::string, std::string>::Decode(in_bytes, in_len, ok, x, y) || !*ok) {
+			return false;
+		}
+
+		// Now we cast the function pointer func_ptr to its original type.
+		//
+		// This incomplete solution only works for this type of member functions.
+		using FunctionPointerType = RT (Svc::*)(std::string, std::string);
+		auto p = func_ptr.To<FunctionPointerType>();
+
+		// EXECUTION STEP: we call the function of the Svc class here and get the result and encode the result
+		RT result = (((Svc *) instance)->*p)(x, y); // The svc class here inherits from the service class so typecasting is allowed
+
+		if (!Protocol<RT>::Encode(out_bytes, out_len, result)) {
+			// out_len should always be large enough so this branch shouldn't be
+			// taken. However just in case, we return an fatal error by setting *ok
+			// to false.
+			*ok = false;
+			return false;
+		}
+		return true;
    }
 };
 
@@ -285,10 +489,32 @@ protected:
    void Export(int (Svc::*func)(int)) {
 		ExportRaw(MemberFunctionPtr::From(func), new IntIntProcedure<Svc>());
    }
+
+   template<typename RT, typename T1>
+   void Export(RT (Svc::*func)(T1)){
+	   ExportRaw(MemberFunctionPtr::From(func), new UniversalProcedure<Svc, RT, T1>());
+   }
+
+   template<typename RT, typename T1, typename T2>
+   void Export(RT (Svc::*func)(T1, T2)){
+	   ExportRaw(MemberFunctionPtr::From(func), new UniversalProcedure<Svc, RT, T1, T2>());
+   }
+
+   template<void, typename T1>
+   void Export(void (Svc::*func)(T1)){
+	   ExportRaw(MemberFunctionPtr::From(func), new UniversalProcedure<Svc, void, T1>());
+   }
+
+   template<void, typename T1, typename T2>
+   void Export(void (Svc::*func)(T1, T2)){
+	   ExportRaw(MemberFunctionPtr::From(func), new UniversalProcedure<Svc, void, T1, T2>());
+   }
+   
    /* add this */
    template<typename RT, typename ... Args>
    void Export(RT (Svc::*func)(Args ...)) {
-	   ExportRaw(MemberFunctionPtr::From(func), new UniversalProcedure<Svc, RT, Args ...>());
+	//    ExportRaw(MemberFunctionPtr::From(func), new UniversalProcedure<Svc, RT, Args ...>());
+	cout << "unimplemented"
    }
    /* end here */
 };
@@ -299,3 +525,60 @@ protected:
  
 
 
+
+
+// Deleted server side code
+
+
+// // TASK2: Server-side: Trying to make universal procedure (probably not for strings)
+// template <typename Svc, typename RT, typename ... Args>
+// class UniversalProcedure : public BaseProcedure {
+
+// 	template<typename T, typename ... Vargs, typename ... FinalArgs> 
+// 	bool DecodeRecursive(uint8_t *in_bytes, uint32_t *in_len,
+//                        uint8_t *out_bytes, uint32_t *out_len,
+//                        bool *ok,  FinalArgs ... finalargs)  {  // passing by reference here?
+//                         //override means don’t allow derived class to override the base class’ virtual function
+// 		T x;
+// 		// This function is similar to Decode. We need to return false if buffer
+// 		// isn't large enough, or fatal error happens during parsing.
+// 		if (!ProtocolUniversal<T>::Decode(in_bytes, in_len, ok, x) || !*ok) {
+// 			return false;
+// 		}
+// 		return DecodeRecursive<Vargs ...>(uint8_t *in_bytes, uint32_t *in_len,
+//                        uint8_t *out_bytes, uint32_t *out_len,
+//                        bool *ok, finalargs ..., x);
+//    }
+
+// 	template<> 
+//     bool DecodeRecursive(uint8_t *in_bytes, uint32_t *in_len,
+//                        uint8_t *out_bytes, uint32_t *out_len,
+//                        bool *ok, ... &finalargs)  { 
+
+// 		using FunctionPointerType = RT (Svc::*)(finalargs ...);
+// 		auto p = func_ptr.To<FunctionPointerType>();
+
+// 		// EXECUTION STEP: we call the function of the Svc class here and get the result and encode the result
+// 		RT result = (((Svc *) instance)->*p)(x); // The svc class here inherits from the service class so typecasting is allowed
+		
+// 		if (!Protocol<RT>::Encode(out_bytes, out_len, result)) {
+// 			// out_len should always be large enough so this branch shouldn't be
+// 			// taken. However just in case, we return an fatal error by setting *ok
+// 			// to false.
+// 			*ok = false;
+// 			return false;
+// 		}
+// 		return true;
+//    }
+   		
+//    bool DecodeAndExecute(uint8_t *in_bytes, uint32_t *in_len,
+//                        uint8_t *out_bytes, uint32_t *out_len,
+//                        bool *ok) override final { 
+
+// 		return DecodeRecursive<Args ...>(uint8_t *in_bytes, uint32_t *in_len,
+//                        uint8_t *out_bytes, uint32_t *out_len,
+//                        bool *ok);
+
+		
+//    }
+// };
