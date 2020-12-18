9de2e0e6274fc0ad502c8742a4e97c38acd8aab7
nadiakostiv <nadiakostiv98@gmail.com>
2020-12-18 00:26:15 -0500

    Re-adding complex changes + string protocol seg fault

--- asst4/rpcxx.h
+++ asst4/rpcxx.h
@@ -2,6 +2,8 @@
 #ifndef RPCXX_H
 #define RPCXX_H
 
+#include <iostream>
+#include <typeinfo>
 #include <cstdlib>
 #include "rpc.h"
 
@@ -61,48 +63,47 @@ template <>
 struct Protocol<std::string> {
     static constexpr size_t TYPE_SIZE = sizeof(std::string);
 
-    static bool Encode(uint8_t *out_bytes, uint32_t *out_len, const std::string  &x) {
-	return false;
+    static bool Encode(uint8_t *out_bytes, uint32_t *out_len, const std::string &x) {
+	int num_bytes = x.length();
+//	std::cout << "sizeof(num_bytes): " << sizeof(int) << std::endl;
+	// check if buffer is big enough to fit the data, if not, return false
+        if (*out_len < (num_bytes+4)) return false;
+
+	// Copy the size of the string into the biffer
+	memcpy(out_bytes, &(num_bytes), sizeof(int));
+	
+	// Create a temporary string
+	char* temp;
+	memcpy(&temp, &x, num_bytes);
+	memcpy(out_bytes+4, temp, num_bytes);
+	
+        *out_len = num_bytes+4;
+	std::cout << "out len: " << *out_len << std::endl;
+        return true;
     }
 
     static bool Decode(uint8_t *in_bytes, uint32_t *in_len, bool *ok, std::string &x) {
-	return false;
+	int num_bytes = 0;
+	std::cout << "Inside decode" << std::endl;
+	memcpy(&num_bytes, in_bytes, 4); //get string length
+	// check if buffer is big enough to read in x, if not, return false
+
+	std::cout << "DECODE: in_len: " << *in_len << ", num_bytes: " << num_bytes << std::endl;
+
+        if (*in_len < (num_bytes+4)) return false;
+
+        // do a memory copy from the buffer into the data, TYPE_SIZE is the size of the data
+	char* temp;
+	memcpy(&temp, in_bytes+4, num_bytes);
+	memcpy(&x, temp, num_bytes);
+	std::cout << "DECODE: string is " << x << std::endl;
+
+        // since we consumed TYPE_SIZE number of bytes from the buffer, we set *in_len to TYPE_SIZE
+        *in_len = num_bytes+4;
+
+        return true;
     }
 };
-
-
-/*
-template <> 
-struct Protocol<int> {
-    static constexpr size_t TYPE_SIZE = sizeof(int);
-
-    static bool Encode(uint8_t *out_bytes, uint32_t *out_len, const int &x) {
-		// check if buffer is big enough to fit the data, if not, return false
-		if (*out_len < TYPE_SIZE) return false; 
-		
-		// do a memory copy of the data into the buffer, TYPE_SIZE is the size of the data
-		memcpy(out_bytes, &x, TYPE_SIZE);
-		
-		// since we wrote TYPE_SIZE number of bytes to the buffer, we set *out_len to TYPE_SIZE
-		*out_len = TYPE_SIZE;
-
-		return true;
-    }
-    
-    static bool Decode(uint8_t *in_bytes, uint32_t *in_len, bool *ok, int &x) {
-		// check if buffer is big enough to read in x, if not, return false
-		if (*in_len < TYPE_SIZE) return false;
-		
-		// do a memory copy from the buffer into the data, TYPE_SIZE is the size of the data
-		memcpy(&x, in_bytes, TYPE_SIZE);
-		
-		// since we consumed TYPE_SIZE number of bytes from the buffer, we set *in_len to TYPE_SIZE
-		*in_len = TYPE_SIZE;
-		
-		return true;
-    }
-};
-*/
 
 // TASK2: Client-side
 class IntParam : public BaseParams {
@@ -155,6 +156,16 @@ class IntResult : public BaseResult {
     int &data() { return r; }
 };
 
+template<typename T>
+class Result {
+  T r;
+public:
+  T &data() { return r; }
+};
+
+template<>
+class Result<void> {};
+
 // TASK2: Client-side
 class Client : public BaseClient {
  public:
@@ -177,6 +188,16 @@ class Client : public BaseClient {
 	}
 	return result;
     }
+
+    /* add this */
+    template<typename Svc, typename RT, typename ... FA> 
+    Result<RT> * Call(Svc *svc, RT (Svc::*f)(FA...), ...) {
+      std::cout << "WARNING: Calling " 
+            << typeid(decltype(f)).name()
+            << " is not supported\n";
+      return nullptr;
+    }
+    /* end here */
 };
 
 // TASK2: Server-side
@@ -186,6 +207,14 @@ class Service : public BaseService {
   	void Export(int (Svc::*func)(int)) {
 	ExportRaw(MemberFunctionPtr::From(func), new IntIntProcedure<Svc>());
     }
+    /* add this */
+    template<typename MemberFunction>
+    void Export(MemberFunction f) {
+      std::cout << "WARNING: Exporting " 
+                << typeid(MemberFunction).name()
+                << " is not supported\n";
+    }
+    /* end here */
 };
 
 }
